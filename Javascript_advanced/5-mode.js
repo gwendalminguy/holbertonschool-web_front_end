2fc1cb955d950eec7c41c459a89e267f66391a8d
Gwendal <gwendalminguy@outlook.fr>
2026-07-10 12:52:24 +0200

    Create script to add mode buttons to a page

---
+++ Javascript_advanced/5-mode.js
@@ -0,0 +1,34 @@
+function changeMode(size, weight, transform, background, color) {
+    document.body.style.fontSize = size;
+    document.body.style.fontWeight = weight;
+    document.body.style.textTransform = transform;
+    document.body.style.backgroundColor = background;
+    document.body.style.color = color;
+}
+
+function main() {
+    let spooky = () => { changeMode(9, "bold", "uppercase", "pink", "green") };
+    let darkMode = () => { changeMode(12, "bold", "capitalize", "black", "white") };
+    let screamMode = () => { changeMode(12, "normal", "lowercase", "white", "black") };
+
+    const textParagraph = document.createElement("p");
+    textParagraph.innerHTML = "Welcome Holberton!";
+    document.body.appendChild(textParagraph);
+
+    const spookyButton = document.createElement("button");
+    spookyButton.innerHTML = "Spooky";
+    spookyButton.addEventListener("click", () => { spooky() });
+    document.body.appendChild(spookyButton);
+
+    const darkModeButton = document.createElement("button");
+    darkModeButton.innerHTML = "Dark Mode";
+    darkModeButton.addEventListener("click", () => { darkMode() });
+    document.body.appendChild(darkModeButton);
+
+    const screamModeButton = document.createElement("button");
+    screamModeButton.innerHTML = "Scream Mode";
+    screamModeButton.addEventListener("click", () => { screamMode() });
+    document.body.appendChild(screamModeButton);
+}
+
+main();
