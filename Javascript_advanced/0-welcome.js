bff9f93e9c52f6aa4587c6162ba7b6493dab66c8
Gwendal <gwendalminguy@outlook.fr>
2026-07-10 10:06:06 +0200

    Replace console.log with alert

--- Javascript_advanced/0-welcome.js
+++ Javascript_advanced/0-welcome.js
@@ -2,7 +2,7 @@ function welcome(firstName, lastName) {
     let fullName = `${firstName} ${lastName}`;
 
     function displayFullName() {
-        console.log(`Welcome ${fullName}!`);
+        alert(`Welcome ${fullName}!`);
     }
 
     displayFullName();
