d13430ffb35b4d6e54e2fbef87097f97fef5b78e
Gwendal <gwendalminguy@outlook.fr>
2026-07-11 01:25:45 +0200

    Create function finding all primes below 100 a hundred times at the end of the execution stack

---
+++ Javascript_advanced/11-prime.js
@@ -0,0 +1,33 @@
+function countPrimeNumbers() {
+    let count = 0;
+    let prime;
+
+    for (let i = 2 ; i < 100 ; i++) {
+        prime = true;
+
+        for (let j = 2 ; j <= Math.sqrt(i) ; j++) {
+            if (i % j === 0) {
+                prime = false;
+                break;
+            }
+        }
+
+        if (prime === true) {
+            count++;
+        }
+    }
+
+    return count;
+}
+
+const now = performance.now();
+
+setTimeout(() => {
+    for (let n = 0 ; n < 100 ; n++) {
+        countPrimeNumbers();
+    }
+}, 0);
+
+const duration = performance.now() - now;
+
+console.log(`Execution time of calculating prime numbers 100 times was ${duration} milliseconds.`)
