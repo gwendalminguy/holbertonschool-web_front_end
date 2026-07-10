c1fc145ce2a96b033427533a72c1eb0bbc2d9cf2
Gwendal <gwendalminguy@outlook.fr>
2026-07-10 13:50:52 +0200

    Add 'return' to implement closures properly

--- Javascript_advanced/5-mode.js
+++ Javascript_advanced/5-mode.js
@@ -7,9 +7,9 @@ function changeMode(size, weight, transform, background, color) {
 }
 
 function main() {
-    let spooky = () => { changeMode(9, "bold", "uppercase", "pink", "green") };
-    let darkMode = () => { changeMode(12, "bold", "capitalize", "black", "white") };
-    let screamMode = () => { changeMode(12, "normal", "lowercase", "white", "black") };
+    let spooky = () => { return changeMode(9, "bold", "uppercase", "pink", "green") };
+    let darkMode = () => { return changeMode(12, "bold", "capitalize", "black", "white") };
+    let screamMode = () => { return changeMode(12, "normal", "lowercase", "white", "black") };
 
     const textParagraph = document.createElement("p");
     textParagraph.textContent = "Welcome Holberton!";
